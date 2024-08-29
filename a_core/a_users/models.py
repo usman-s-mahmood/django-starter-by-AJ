8f615fe5dfe4aa3e4ddf54b0f0c40e7e6200e5ba
usman.shahid <usmanshahid027@outlook.com>
2024-08-29 12:28:13 -0700

    created Profile Model for user profile handling

--- a_core/a_users/models.py
+++ a_core/a_users/models.py
@@ -4,3 +4,22 @@ from django.templatetags.static import static
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True
+    )
+    displayname = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    info = models.TextField(
+        null=True,
+        blank=True
+    )
