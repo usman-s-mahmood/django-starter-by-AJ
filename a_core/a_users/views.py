e809d2155694d79cbecfaa91c6c1e7a250fe4b96
usman.shahid <usmanshahid027@outlook.com>
2024-08-29 13:18:33 -0700

    added profile_view to a_users

--- a_core/a_users/views.py
+++ a_core/a_users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile_view(request):
+    profile = request.user.profile
+    return render(
+        request,
+        'a_users/profile.html',
+        {
+            'profile': profile
+        }
+    )
